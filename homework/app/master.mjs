590b45f31ef8d0dc1e11c8fee9baee87a6238e8d
Eugene Kostrov <1741779+eawer@users.noreply.github.com>
2020-12-31 00:38:31 +0200

    homework 3 added

--- homework/app/master.mjs
+++ homework/app/master.mjs
@@ -1,85 +1,192 @@
 import url from 'url';
 import http from 'http';
+import querystring from 'querystring';
 
 
-const MESSAGES = [];
-const { EXTERNAL_PORT_MASTER, INTERNAL_PORT_SEC, SECONDARY_COUNT, TIMEOUT } = process.env;
+const {
+    EXTERNAL_PORT_MASTER, INTERNAL_PORT_SEC,
+    SECONDARY_COUNT, TIMEOUT, MAX_RETRY_TIMEOUT,
+    RETRY_TIMEOUT,
+} = process.env;
 
 
-const spreadMessages = async (req, res) => {
-    const { query } = url.parse(req.url, true);
-    const { message, w } = query;
-    
-    const acksNeeded = w || +SECONDARY_COUNT + 1;
-    let acksReceived = 0;
+class Master {
+    constructor(config) {
+        this.port = +config.port;
+        this.timeout = +config.timeout;
+        this.retryTimeout = +config.retryTimeout;
+        this.maxRetryTimeout = +config.maxRetryTimeout;
+        this.secondariesCount = +config.secondaries.count;
+        this.secondariesPort = +config.secondaries.port;
+        this.secondaries = this.constructor.getSecondariesNames(this.secondariesCount);
+        this.health = this.secondaries.reduce((acc, curr) => {
+            acc[curr] = {
+                up: true,
+                retries: 0,
+            };
+            return acc;
+        }, {});
+        this.messages = [];
+        this.init();
+    }
 
-    MESSAGES.push(message);
-    acksReceived += 1;
+    static getSecondariesNames(count) {
+        const names = [];
+        for (let i = 1; i <= count; i++) {
+            names.push(`http://secondary_${i}`);
+        }
+        return names;
+    }
 
-    const options = {
-        port: INTERNAL_PORT_SEC,
-        path: `/?message=${message}`,
-        method: 'POST',
+    static wait(ms) {
+        return new Promise((resolve) => {
+            setTimeout(resolve, ms);
+        });
+    }
+
+    healthOverlord() {
+        const { health } = this;
+        const options = {
+            port: this.secondariesPort,
+            path: '/health',
+            method: 'POST',
+            timeout: this.timeout,
+        };
+        setInterval(() => {
+            for (const secondary of this.secondaries) {
+                const request = http.request(secondary, options, (response) => {
+                    const success = response.statusCode == 200;
+
+                    if (!success) {
+                        console.log(`${secondary} is sick. Number of unsuccessful retries is ${health[secondary].retries + 1}`)
+                    } else if (success && !health[secondary].up) {
+                        console.log(`${secondary} is healthy again.`)
+                    }
+
+                    health[secondary].up = success;
+                    health[secondary].retries = success ? 0 : health[secondary].retries + 1;
+                });
+                request.on("error", () => {
+                    health[secondary].up = false;
+                    health[secondary].retries += 1;
+                    console.log(`${secondary} is sick. Number of unsuccessful retries is ${health[secondary].retries}`)
+                    request.abort();
+                });
+                request.end();
+            }
+        }, this.retryTimeout);
+    }
+
+
+    async sendMessage(url, options, callback) {
+        const { health } = this;
+        let delivered = false;
+
+        while (!delivered) {
+            delivered = await new Promise((resolve) => {
+                const request = http.request(url, options, (response) => {
+                    resolve(response.statusCode == 200);
+                });
+                request.on("error", (e) => {
+                    request.abort();
+                    resolve(false);
+                });
+                request.end();
+            });
+
+            await this.constructor.wait(this.retryTimeout + Math.min(this.maxRetryTimeout, this.retryTimeout*health[url].retries));
+        }
+        callback(delivered);
+    }
+
+    async spreadMessages (req, res) {
+        const { query } = url.parse(req.url, true);
+        const { m, w } = query;
+
+        const acksNeeded =  w || +this.secondariesCount + 1;
+        let acksReceived = 0;
+
+        const message = querystring.stringify({
+            body: m,
+            timestamp: this.messages.length + 1,
+        });
+
+        this.messages.push(m);
+        acksReceived += 1;
+
+        const options = {
+            port: this.secondariesPort,
+            path: `/message?${message}`,
+            timeout: this.timeout,
+            method: 'POST',
+        };
+
+        try {
+            await new Promise((resolve, reject) => {
+                if (acksReceived == acksNeeded) {
+                    resolve();
+                }
+
+                for (const secondary of this.secondaries) {
+                    this.sendMessage(secondary, options, (delivered) => {
+                        if (delivered) {
+                            acksReceived += 1;
+                        }
+                        if (acksReceived == acksNeeded) {
+                            resolve();
+                        }
+                    });
+                }
+            })
+        } catch (e) {
+            console.log(e)
+        } finally {
+            if (acksReceived == acksNeeded) {
+                res.writeHead(200);
+                res.end(`Message "${m}" was delivered after ${acksNeeded} acks`);
+            } else {
+                res.writeHead(500);
+                res.end(`Message "${m}" was not delivered`);
+            }
+        }
     };
 
-    try {
-        await new Promise((resolve, reject) => {
-            if (acksReceived == acksNeeded) {
-                resolve();
-            }
-    
-            for (let i = 1; i <= SECONDARY_COUNT; i++) {
-                const secondaryRequest = http.request(`http://secondary_${i}`, options, (internalResponse) => {
-                    if (internalResponse.statusCode == 200) {
-                        acksReceived += 1;
-                    }
-                    if (acksReceived == acksNeeded) {
-                        resolve();
-                    }
-                });
+    async router(req, res) {
+        const { method: METHOD } = req;
 
-                setTimeout(() => {
-                    secondaryRequest.abort();
-                    reject(new Error("Timeout"));
-                }, TIMEOUT);
-
-                secondaryRequest.on("error", (e) => {
-                    console.log(e);
-                });
-
-                secondaryRequest.end();
-            }
-        })
-    } catch (e) {
-        console.log(e)
-    } finally {
-        if (acksReceived == acksNeeded) {
-            res.writeHead(200);
-            res.end(`Message "${message}" was delivered after ${acksNeeded} acks`);
-        } else {
-            res.writeHead(500);
-            res.end(`Message "${message}" was not delivered`);
+        switch (METHOD) {
+            case "POST":
+                console.log("POST");
+                this.spreadMessages(req, res);
+                break;
+            case "GET":
+                console.log("GET");
+                res.writeHead(200);
+                res.end(this.messages.map(m => m.body).join(", "));
+                break;
         }
     }
-};
 
-
-const requestListener = async (req, res) => {
-    const { method: METHOD } = req;
-
-    switch (METHOD) {
-        case "POST":
-            console.log("POST");            
-            spreadMessages(req, res);
-            break;
-        case "GET":
-            console.log("GET");
-            res.writeHead(200);
-            res.end(MESSAGES.join(", "));
-            break;
+    init() {
+        const server = http.createServer(this.router.bind(this));
+        server.listen(this.port);
+        this.healthOverlord();
     }
-};
-  
-const server = http.createServer(requestListener);
-server.listen(EXTERNAL_PORT_MASTER);
+}
 
+
+
+try {
+    new Master({
+        port: EXTERNAL_PORT_MASTER,
+        timeout: TIMEOUT,
+        retryTimeout: RETRY_TIMEOUT,
+        maxRetryTimeout: MAX_RETRY_TIMEOUT,
+        secondaries: {
+            count: SECONDARY_COUNT,
+            port: INTERNAL_PORT_SEC,
+        },
+    });
+} catch (e) {
+    console.log(e)
+}
--- homework/app/secondary.mjs
+++ homework/app/secondary.mjs
@@ -2,43 +2,123 @@ import url from 'url';
 import http from 'http';
 
 
-const MESSAGES = [];
 const MIN_DELAY = 1000;
-const MAX_DELAY = 4000;
+const MAX_DELAY = 20000;
 const { INTERNAL_PORT_SEC, EXTERNAL_PORT_SEC } = process.env;
 
-const internalListener = (req, res) => {
-    const DELAY = Math.floor(Math.random() * (MAX_DELAY - MIN_DELAY)) + MIN_DELAY;
 
-    const { method: METHOD } = req;
-    const { query } = url.parse(req.url, true);
+class Secondary {
+    constructor(config) {
+        this.internalPort = config.internalPort;
+        this.externalPort = config.externalPort;
+        this.minDelay = config.minDelay;
+        this.maxDelay = config.maxDelay;
+        this.buffer = [];
+        this.messages = [];
+        this.init();
+    }
 
-    if (METHOD === "POST") {
-        MESSAGES.push(query["message"]);
+    static sortMessages(array) {
+        return array.sort((a, b) => a.timestamp - b.timestamp);
+    }
+
+    checkIfMessageIsNext(message = null) {
+        const { buffer, messages } = this;
+        const lastMessageTimestamp = messages.length ? messages[messages.length - 1].timestamp : 0;
+        const messageToCheck = message ? message : this.constructor.sortMessages(buffer)[0];
+
+        return messageToCheck.timestamp - lastMessageTimestamp == 1;
+    }
+
+    bufferOverlord() {
+        const { buffer, messages } = this;
+        setInterval(() => {
+            if (buffer.length && this.checkIfMessageIsNext()) {
+                const message = buffer.shift()
+                messages.push(message);
+                console.log(`Message "${message.body}" with timestamp=${message.timestamp} was delivered from buffer.`);
+            }
+        }, 1000);
+    }
+
+    messageIsPresent(message, messages) {
+        for (const item of messages) {
+            if (item.timestamp == message.timestamp) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    handleMessage(message, res) {
+        const { buffer, messages } = this;
+        const { body, timestamp } = message;
+        const DELAY = Math.floor(Math.random() * (this.maxDelay - this.minDelay)) + this.minDelay;
+
         setTimeout(() => {
-            console.log(`received with ${DELAY} delay`);
+            let logMessage = `Message "${body}" with timestamp=${timestamp} was received `;
+
+            if (this.messageIsPresent(message, this.messages)) {
+                logMessage += "(DUP!)."
+            } else if (!this.checkIfMessageIsNext(message) && !this.messageIsPresent(message, this.buffer)) {
+                buffer.push(message);
+                this.constructor.sortMessages(buffer);
+                logMessage += "and put into buffer."
+            } else {
+                messages.push(message);
+                logMessage += `with delay=${DELAY} and delivered.`
+            }
+
+            console.log(logMessage);
             res.writeHead(200);
             res.end();
-        }, DELAY);    
-    } else {
-        res.writeHead(400);
-        res.end();
+        }, DELAY);
+    }
+
+    async internalRouter(req, res) {
+        const { method: METHOD } = req;
+        const { query, pathname } = url.parse(req.url, true);
+
+        if (METHOD === 'POST' && pathname == '/message') {
+            this.handleMessage(query, res);
+        } else {
+            const statusCode = (pathname == '/health') ? 200 : 400;
+            res.writeHead(statusCode);
+            res.end();
+        }
+    }
+
+    async externalRouter(req, res) {
+        const { method: METHOD } = req;
+
+        if (METHOD === "GET") {
+            console.log(METHOD);
+            res.writeHead(200);
+            res.end(this.messages.map(m => m.body).join(", "));
+        } else {
+            console.error(METHOD);
+            res.writeHead(400);
+            res.end("Illegal request");
+        }
+    }
+
+    init() {
+        http.createServer(this.internalRouter.bind(this)).listen(this.internalPort);
+        http.createServer(this.externalRouter.bind(this)).listen(this.externalPort);
+        this.bufferOverlord();
     }
 }
 
-const externalListener = (req, res) => {
-    const { method: METHOD } = req;
-    
-    if (METHOD === "GET") {
-        console.log(METHOD);
-        res.writeHead(200);
-        res.end(MESSAGES.join(", "));
-    } else {
-        console.error(METHOD);
-        res.writeHead(400);
-        res.end("Illegal request");
-    }
+
+
+try {
+    new Secondary({
+        internalPort: INTERNAL_PORT_SEC,
+        externalPort: EXTERNAL_PORT_SEC,
+        minDelay: MIN_DELAY,
+        maxDelay: MAX_DELAY,
+    });
+} catch (e) {
+    console.log(e)
 }
 
-http.createServer(internalListener).listen(INTERNAL_PORT_SEC);
-http.createServer(externalListener).listen(EXTERNAL_PORT_SEC);
